69bfa2453e904123d4a3b77d8785b93464251d80
Carlos Butron <carlos@mailfence.com>
2022-03-10 01:26:33 +0100

    Add helper to handle search footer info view

---
+++ MarvelChallenge/Helpers/SearchFooter.swift
@@ -0,0 +1,65 @@
+//
+//  SearchFooter.swift
+//  MarvelChallenge
+//
+//  Created by Carlos Butrón on 10/3/22.
+//
+
+import UIKit
+
+class SearchFooter: UIView {
+    let label = UILabel()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureView()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configureView()
+    }
+
+    override func draw(_ rect: CGRect) {
+        label.frame = bounds
+    }
+
+    func setNotFiltering() {
+        label.text = ""
+        hideFooter()
+    }
+
+    func updateFiltering(filteredItemCount: Int, of totalItemCount: Int) {
+        label.text = String(format: "charactersFooterCount".localized, filteredItemCount, totalItemCount)
+    }
+
+    func setIsFilteringToShow(filteredItemCount: Int, of totalItemCount: Int) {
+        if (filteredItemCount == 0) {
+            label.text = "noItemsMatch".localized
+            showFooter()
+        } else {
+            label.text = String(format: "charactersFooterCount".localized, filteredItemCount, totalItemCount)
+            showFooter()
+        }
+    }
+
+    private func hideFooter() {
+        UIView.animate(withDuration: 0.7) {
+            self.alpha = 0.0
+        }
+    }
+
+    private func showFooter() {
+        UIView.animate(withDuration: 0.7) {
+            self.alpha = 1.0
+        }
+    }
+
+    private func configureView() {
+        backgroundColor = ThemeColor.blackMV.MVColor
+        alpha = 0.0
+        label.textAlignment = .center
+        label.textColor = .white
+        addSubview(label)
+    }
+}
